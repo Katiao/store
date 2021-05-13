caab67efd35e3b8f7667afb3ca16167e4d91b730
Katia <katiao_@hotmail.co.uk>
2021-05-13 11:01:15 +0200

    setup Products Context and Reducer, add sidebar toggle functionality.

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -13,18 +13,34 @@ import {
 	GET_SINGLE_PRODUCT_ERROR,
 } from '../actions';
 
-const initialState = {};
+const initialState = {
+	isSidebarOpen: false,
+};
 
 const ProductsContext = React.createContext();
+//we wrapped our whole App in index.js in the ProductsContext so that other components have access to state and props.
+//we also use use reducer here to update state. To change state we' always have to run the type of action through our dispatch.
+// you can only adapt the state if there is an action.
+//reducer that's passed in to useReducer comes from reducer file
 
 export const ProductsProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const openSidebar = () => {
+		dispatch({ type: SIDEBAR_OPEN });
+	};
+
+	const closeSidebar = () => {
+		dispatch({ type: SIDEBAR_CLOSE });
+	};
+
 	return (
-		<ProductsContext.Provider value='products context'>
+		<ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
 			{children}
 		</ProductsContext.Provider>
 	);
 };
-// make sure use
+// This is my hook which ensures that I can use the context other components:
 export const useProductsContext = () => {
 	return useContext(ProductsContext);
 };
