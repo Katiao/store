cffd6b2910fca15d18747ffe64d628ef3b66ee47
Katia <katiao_@hotmail.co.uk>
2021-06-02 11:29:20 +0200

    set up netlify-cli and stripe checkout

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-/* import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import {
 	CardElement,
 	useStripe,
 	Elements,
 	useElements,
-} from '@stripe/react-stripe-js'; */
-//import axios from 'axios';
+} from '@stripe/react-stripe-js';
+import axios from 'axios';
 import { useCartContext } from '../context/cart_context';
 import { useUserContext } from '../context/user_context';
 import { formatPrice } from '../utils/helpers';
 import { useHistory } from 'react-router-dom';
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
 	return <h4>hello from Stripe Checkout </h4>;
@@ -20,7 +22,9 @@ const CheckoutForm = () => {
 const StripeCheckout = () => {
 	return (
 		<Wrapper>
-			<CheckoutForm />
+			<Elements stripe={promise}>
+				<CheckoutForm />
+			</Elements>
 		</Wrapper>
 	);
 };
