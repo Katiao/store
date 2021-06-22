bb83110a596a8f86cfdbccf2ddb999fc4468aac0
unknown <katiao_@hotmail.co.uk>
2021-06-22 15:24:32 +0200

    fix API reading bug on single product page

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useReducer } from 'react';
-import reducer from '../reducers/products_reducer';
-import { products_url as url } from '../utils/constants';
+import axios from "axios";
+import React, { useContext, useEffect, useReducer } from "react";
+import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
 import {
-	SIDEBAR_OPEN,
-	SIDEBAR_CLOSE,
-	GET_PRODUCTS_BEGIN,
-	GET_PRODUCTS_SUCCESS,
-	GET_PRODUCTS_ERROR,
-	GET_SINGLE_PRODUCT_BEGIN,
-	GET_SINGLE_PRODUCT_SUCCESS,
-	GET_SINGLE_PRODUCT_ERROR,
-} from '../actions';
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+} from "../actions";
 
 const initialState = {
-	isSidebarOpen: false,
-	products_loading: false,
-	products_error: false,
-	products: [],
-	featured_products: [],
-	single_product_loading: false,
-	single_product_error: false,
-	single_product: {},
+  isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -31,53 +31,54 @@ const ProductsContext = React.createContext();
 //reducer that's passed in to useReducer comes from reducer file
 
 export const ProductsProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-	const openSidebar = () => {
-		dispatch({ type: SIDEBAR_OPEN });
-	};
+  const openSidebar = () => {
+    dispatch({ type: SIDEBAR_OPEN });
+  };
 
-	const closeSidebar = () => {
-		dispatch({ type: SIDEBAR_CLOSE });
-	};
+  const closeSidebar = () => {
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
 
-	//fetching products from API. Setting up useEffect in context, because I can fetch it once and can distribute it to home page and products page.
-	const fetchProducts = async (url) => {
-		//this dispatch will set up the loading in products reducer. Handles loading, sucess and error.
-		dispatch({ type: GET_PRODUCTS_BEGIN });
-		try {
-			const response = await axios.get(url);
-			const products = response.data;
-			dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
-		} catch (error) {
-			dispatch({ type: GET_PRODUCTS_ERROR });
-		}
-	};
+  //fetching products from API. Setting up useEffect in context, because I can fetch it once and can distribute it to home page and products page.
+  const fetchProducts = async (url) => {
+    //this dispatch will set up the loading in products reducer. Handles loading, sucess and error.
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
 
-	const fetchSingleProduct = async (url) => {
-		dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
-		try {
-			const response = await axios.get(url);
-			console.log(response.data)
-			const singleProduct = response.data;
-			dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
-		} catch (error) {
-			dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
-		}
-	};
-	//fetchsingleProduct will be passed in the useEffect of the single product page as we load single products there.
-	useEffect(() => {
-		fetchProducts(url);
-	}, []);
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      console.log(response.data, "single product data PART2");
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
+  //fetchsingleProduct will be passed in the useEffect of the single product page as we load single products there.
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
-	return (
-		<ProductsContext.Provider
-			value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}>
-			{children}
-		</ProductsContext.Provider>
-	);
+  return (
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
 };
 // This is my hook which ensures that I can use the context other components:
 export const useProductsContext = () => {
-	return useContext(ProductsContext);
+  return useContext(ProductsContext);
 };
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,129 +1,125 @@
-import React, { useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { useProductsContext } from '../context/products_context';
-import { single_product_url as url } from '../utils/constants';
-import { formatPrice } from '../utils/helpers';
+import React, { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useProductsContext } from "../context/products_context";
+import { single_product_url as url } from "../utils/constants";
+import { formatPrice } from "../utils/helpers";
 import {
-	Loading,
-	Error,
-	ProductImages,
-	AddToCart,
-	Stars,
-	PageHero,
-} from '../components';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+  Loading,
+  Error,
+  ProductImages,
+  AddToCart,
+  Stars,
+  PageHero,
+} from "../components";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const SingleProductPage = () => {
-	const { id } = useParams();
-	const history = useHistory();
-	const {
-		single_product_loading: loading,
-		single_product_error: error,
-		single_product: product,
-		fetchSingleProduct,
-	} = useProductsContext();
+  const { id } = useParams();
+  const history = useHistory();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
 
-	//when I load the page / useEffect, when component mounts or ID changes:
+  //when I load the page / useEffect, when component mounts or ID changes:
 
-	useEffect(() => {
-		fetchSingleProduct(`${url}${id}`);
-		//eslint-disable-next-line
-	}, [id]);
-	//set up automatic navigation back to home page if there is an error in 3 secs:
-	//we can navigate away from page using useHistory hook
-	useEffect(() => {
-		if (error) {
-			setTimeout(() => {
-				history.push(`/`);
-			}, 3000);
-		}
-		//eslint-disable-next-line
-	}, [error]);
-	if (loading) {
-		return <Loading />;
-	}
-	if (error) {
-		return <Error />;
-	}
-	const {id: sku } = product
-	const { fields } = product
-	const {
-		name,
-		price,
-		description,
-		stock,
-		stars,
-		reviews,
-		brand,
-		images,
-	} = fields
-	 console.log(description)
-	return (
-		<Wrapper>
-			<PageHero title={name} product />
-			<div className='section section-center page'>
-				<Link to='/products' className='btn'>
-					back to products
-				</Link>
-				<div className='product-center'>
-					{/* we have an array of images which we pass in to the product images component */}
-					<ProductImages images={images} />
-				</div>
-				<section className='content'>
-					<h2>{name}</h2>
-					<Stars stars={stars} reviews={reviews} />
-					<h5 className='price'>{formatPrice(price)}</h5>
-					<p className='desc'>{description}</p>
-					<p className='info'>
-						<span> Available : {stock > 0 ? 'In stock' : 'out of stock'} </span>
-					</p>
-					<p className='info'>
-						<span>SKU : {sku} </span>
-					</p>
-					<p className='info'>
-						<span>Brand : {brand} </span>
-					</p>
-					<hr />
-					{stock > 0 && <AddToCart product={product} />}
-				</section>
-			</div>
-		</Wrapper>
-	);
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+    //eslint-disable-next-line
+  }, [id]);
+  //set up automatic navigation back to home page if there is an error in 3 secs:
+  //we can navigate away from page using useHistory hook
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push(`/`);
+      }, 3000);
+    }
+    //eslint-disable-next-line
+  }, [error]);
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+  //if (Object.entries(product).length === 0) return null;
+  if (!Object.entries(product).length) return null;
+  const { id: sku } = product;
+  console.log(product, "this is a product");
+  const { fields } = product;
+  const { name, price, description, stock, stars, reviews, brand, images } =
+    fields;
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          back to products
+        </Link>
+        <div className="product-center">
+          {/*  we have an array of images which we pass in to the product images
+		 component */}
+          <ProductImages images={images} />
+        </div>
+        <section className="content">
+          <h2>{name}</h2>
+          <Stars stars={stars} reviews={reviews} />
+          <h5 className="price">{formatPrice(price)}</h5>
+          <p className="desc">{description}</p>
+          <p className="info">
+            <span> Available : {stock > 0 ? "In stock" : "out of stock"} </span>
+          </p>
+          <p className="info">
+            <span>SKU : {sku} </span>
+          </p>
+          <p className="info">
+            <span>Brand : {brand} </span>
+          </p>
+          <hr />
+          {stock > 0 && <AddToCart product={product} />}
+        </section>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.main`
-	.product-center {
-		display: grid;
-		gap: 4rem;
-		margin-top: 2rem;
-	}
-	.price {
-		color: var(--clr-primary-5);
-	}
-	.desc {
-		line-height: 2;
-		max-width: 45em;
-	}
-	.info {
-		text-transform: capitalize;
-		width: 300px;
-		display: grid;
-		grid-template-columns: 125px 1fr;
-		span {
-			font-weight: 700;
-		}
-	}
+  .product-center {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--clr-primary-5);
+  }
+  .desc {
+    line-height: 2;
+    max-width: 45em;
+  }
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
 
-	@media (min-width: 992px) {
-		.product-center {
-			grid-template-columns: 1fr 1fr;
-			align-items: center;
-		}
-		.price {
-			font-size: 1.25rem;
-		}
-	}
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
 `;
 
 export default SingleProductPage;
